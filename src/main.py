d96a371774f2933073f22ba8c99882ab96cb7c52
yongchang-z <yongchang.v.zhang@gmail.com>
2025-03-06 00:25:12 +1100

    initial setup

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+print("hello world")
+
+
+def main():
+    new_textnode = TextNode("This is some anchor text", "links", "https://www.boot.dev")
+    print(new_textnode)
+
+
+main()
